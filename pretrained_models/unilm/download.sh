57df9a13e1468bfa58b611e1a155d4a0a8a70905
iser97 <1156653418@qq.com>
2023-04-07 11:20:31 +0000

    add dataset modeling

---
+++ pretrained_models/unilm/download.sh
@@ -0,0 +1,1 @@
+wget https://valle.blob.core.windows.net/share/BEATs/BEATs_iter3_plus_AS2M.pt?sv=2020-08-04\&st=2023-03-01T07%3A51%3A05Z\&se=2033-03-02T07%3A51%3A00Z\&sr=c\&sp=rl\&sig=QJXmSJG9DbMKf48UDIU1MfzIro8HQOf3sqlNXiflY1I%3D
